69b18e416c9a394cfb9cdb676a728e61ce475638
Aadhithya Sankar <a.sankar@tum.de>
2018-11-11 00:10:34 +0100

    Url Redirect

--- db_console/analysis.py
+++ db_console/analysis.py
@@ -3,16 +3,19 @@ from termgraph_api.termgraph import *
 import webbrowser
 import datetime
 import sys
+from pprint import pprint
 
 def analyze(filename):
     df = pd.read_csv(filename)
     base_url = 'https://link.bahn.guru/?journey='
     url_end = '&bc=0&class=2'
 
+
     df['departure_time'] = pd.to_datetime(df['departure_time'])
     grouped = df.groupby(df['date']).min()
-    url_list = list(grouped['fulltxt'])
 
+    url_list = list(grouped['fulltxt'])
+    pprint(grouped['fulltxt'])
     print('Ticket Prices:\n')
     dep_time_t = list(grouped['departure_time'])
     dep_time = []
@@ -24,7 +27,7 @@ def analyze(filename):
     print('\n')
 
     print('Type index number to book or -1 to exit: ')
-    ind = input()
+    ind = int(input())
     if ind is -1:
         sys.exit()
     else:
@@ -32,7 +35,10 @@ def analyze(filename):
             redir = url_list[ind]
             url = base_url+redir+url_end
             print('opening db.de in new tab...')
+            print(url_list)
             webbrowser.open_new_tab(url)
-
+        except Exception as e:
+            print('Invalid index entered. Exiting.')
+            sys.exit()
     #plt.show()s
     return True

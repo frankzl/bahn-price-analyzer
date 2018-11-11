8ec93b7832ea40a42415df9f5b65b68e3a6ec72c
Frank Lu <frankz.lu@googlemail.com>
2018-11-11 12:54:00 +0100

    remake

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 echo $SCRIPTPATH
 
-rm -f .config
+# rm -f .config
 touch .config
 
 echo "alias bahn=\"python $SCRIPTPATH/db_console/db_user_program.py\"" >> .config
@@ -13,3 +13,6 @@ echo "alias bahn_info=\"python $SCRIPTPATH/db_console/db_info.py\"" >> .config
 
 echo "source $SCRIPTPATH/.config" >> $HOME/.bashrc
 echo "source $SCRIPTPATH/.config" >> $HOME/.zshrc
+
+source $SCRIPTPATH/.config
+

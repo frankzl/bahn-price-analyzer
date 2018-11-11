3911e42de69349a4b9384a3f3ce9f452a171de33
Frank Lu <frankz.lu@googlemail.com>
2018-11-11 12:49:03 +0100

    added setup

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
+
+rm -f .config
+touch .config
+
+echo "alias bahn=\"python $SCRIPTPATH/db_console/db_user_program.py\"" >> .config
+echo "alias bahn_info=\"python $SCRIPTPATH/db_console/db_info.py\"" >> .config
+
+echo "source $SCRIPTPATH/.config" >> $HOME/.bashrc
+echo "source $SCRIPTPATH/.config" >> $HOME/.zshrc

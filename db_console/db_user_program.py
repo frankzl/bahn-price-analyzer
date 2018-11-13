62148210d6c9a4f65bbbc338f2ca6f52994ec528
Aadhithya Sankar <a.sankar@tum.de>
2018-11-13 15:22:22 +0100

    Bug Fix to handle windoes error

--- db_console/db_user_program.py
+++ db_console/db_user_program.py
@@ -1,14 +1,16 @@
 import os
-import pandas as pd
-import readline
-from StationsCompleter import *
+
 
 if __name__ == "__main__":
 
-    stations = pd.read_csv('stations.csv').values
-    completer = StationsCompleter(stations.T[0])
-    readline.set_completer(completer.complete)
-    readline.parse_and_bind('tab: complete')
+    if 'Windoes' not in platform.platform():
+        import pandas as pd
+        import readline
+        from StationsCompleter import *
+        stations = pd.read_csv('stations.csv').values
+        completer = StationsCompleter(stations.T[0])
+        readline.set_completer(completer.complete)
+        readline.parse_and_bind('tab: complete')
 
     departure = input("Start Station:\t")
     arrival = input("End Station:\t")
@@ -17,7 +19,7 @@ if __name__ == "__main__":
     from_date = input("Begin Date:\t")
     to_date = input("End Date:\t")
 
-    # run command "python db_info.py -f "Oldenburg (Oldb) Hbf" -t München Hbf -fd 2018-11-11 -td 2018-11-20"
+    #run command "python db_info.py -f "Oldenburg (Oldb) Hbf" -t München Hbf -fd 2018-11-11 -td 2018-11-20"
     path = os.path.dirname(os.path.realpath(__file__))
     
     command = "python "+ path +"/db_info.py -f \""+ departure + "\" -t \"" + arrival + "\" -fd " + from_date + " -td " + to_date
